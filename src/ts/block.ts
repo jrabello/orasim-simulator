671e2a115e542bf69419337837a361f67675c16b
jrabello <jrabello@zion>
2016-10-15 05:11:43 -0300

    fixando comentario block

--- src/ts/block.ts
+++ src/ts/block.ts
@@ -3,7 +3,7 @@
  * Classe Responsavel modelar um bloco
  * @attribute {size} define o tamanho de um bloco
  * @attribute {element} objeto html que referencia o elemento user-process
- * @attribute {isUsed} flag que determina se o bloco esta em uso 
+ * @attribute {isUsed} flag que determina se o bloco esta em uso  
  */
 export class Block{
     private size: number    
